7808710023bf390190def560f9b2f934284918cc
LegendaryLuck <noe.boudry@gmail.com>
2020-04-12 12:40:08 +0200

    reinstallation du main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
 });
 
-client.on('message', msg => {
+bot.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
 
-client.login('token');
+bot.login('Njk4ODExNTk0ODM0MDUxMDcy.XpLRIQ.dxD7slh7a0XJAEq2tN_ZfiuUoaM');
